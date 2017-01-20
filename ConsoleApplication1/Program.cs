00eef86baf38b7608b0e051bad5efca0ac69c95b
Ass <asss>
2017-01-20 07:00:13 +0200

    Commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int fdsjbfjdsbfkbdsjbf;
+            int fdsjbfjdsbfkbdsjbf = 0;
         }
     }
 }
